f17304efa5a5412543983559777a91caf8b280ef
Ssemaganda George <ssemagandageorge480@gmail.com>
2023-10-15 12:12:19 +0300

    Updated the setup and main functions.

--- functions/main().c
+++ functions/main().c
@@ -4,36 +4,18 @@
 #include <conio.h>
 #include <stdlib.h>
 
+int i, j, height  =20, width = 20, gameover, score;
+int x, y, fruitx, fruity, flag;
 
-//int i, j, height  =20, width = 20, gameover, score;
-//int x, y, fruitx, fruity, flag;
+void main(){
+    int m, n;
+    setup();
+//Untill game gets over!!
+    while (!gameover){
 
-//Constructing the boundary outline using draw(), tool.
-int i, j, height = 20;
-int width = 20, gameover, score;
-
-//Function to draw the boundary.
-
-void draw(){
-
-    for (i = 0; i < height; i++){
-        for (j = 0; j < width; j ++ ){
-            if (i == 0 || i == width - 1 || j == 0 || j == height - 1){
-                    printf("#");
-            }
-
-            else{
-                    printf(" ");
-            }
-        }
-        printf("\n");
-
+        //Functional calls
+        draw();
+        input();
+        logic();
     }
-}
-//Declaring the main function.
-int main(){
-    //functional  call
-    draw();
-
-    return 0;
 }
--- functions/setup().c
+++ functions/setup().c
@@ -5,35 +5,20 @@
 #include <stdlib.h>
 
 
-//int i, j, height  =20, width = 20, gameover, score;
-//int x, y, fruitx, fruity, flag;
+int i, j, height  =20, width = 20, gameover, score;
+int x, y, fruitx, fruity, flag;
 
-//Constructing the boundary outline using draw(), tool.
-int i, j, height = 20;
-int width = 20, gameover, score;
-
-//Function to draw the boundary.
-
-void draw(){
-
-    for (i = 0; i < height; i++){
-        for (j = 0; j < width; j ++ ){
-            if (i == 0 || i == width - 1 || j == 0 || j == height - 1){
-                    printf("#");
-            }
-
-            else{
-                    printf(" ");
-            }
-        }
-        printf("\n");
-
-    }
-}
-//Declaring the main function.
-int main(){
-    //functional  call
-    draw();
-
-    return 0;
+void setup() {
+    gameover = 0;
+    x = height / 2;
+    y = width / 2;
+    label1:
+        fruitx = rand() %20;
+        if(fruitx == 0)
+            goto label1;
+    label2:
+        fruity = rand() %20;
+        if(fruity == 0)
+            goto label2;
+    score = 0;
 }
