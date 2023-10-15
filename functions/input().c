815b32d583cf12b0600f87136bae40a6384a8c88
Ssemaganda George <ssemagandageorge480@gmail.com>
2023-10-15 12:20:48 +0300

    Upated the input files.

--- functions/input().c
+++ functions/input().c
@@ -5,35 +5,27 @@
 #include <stdlib.h>
 
 
-//int i, j, height  =20, width = 20, gameover, score;
-//int x, y, fruitx, fruity, flag;
+int i, j, height  =20, width = 20, gameover, score;
+int x, y, fruitx, fruity, flag;
 
-//Constructing the boundary outline using draw(), tool.
-int i, j, height = 20;
-int width = 20, gameover, score;
-
-//Function to draw the boundary.
-
-void draw(){
-
-    for (i = 0; i < height; i++){
-        for (j = 0; j < width; j ++ ){
-            if (i == 0 || i == width - 1 || j == 0 || j == height - 1){
-                    printf("#");
-            }
-
-            else{
-                    printf(" ");
-            }
+void input(){
+    if(kbhit()){
+        switch(getch()){
+            case 'a':
+                flag = 1;
+                break;
+            case 's':
+                flag = 2;
+                break;
+            case 'd':
+                flag = 3;
+                break;
+            case 'w':
+                flag = 4;
+                break;
+            case 'x':
+                gameover = 1;
+                break;
         }
-        printf("\n");
-
     }
-}
-//Declaring the main function.
-int main(){
-    //functional  call
-    draw();
-
-    return 0;
 }
