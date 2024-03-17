6fb2b7eba3c7d9e8e099dd1d57129ecae1ae1218
JoJ <ssemagandageorge480@gmail.com>
2024-03-17 10:23:24 +0300

    Refactored the parameters at main().c

--- functions/main().c
+++ functions/main().c
@@ -1,13 +1,15 @@
-//declaring library packages.
+//Including the required library packages.
 
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
 #include <windows.h> // For kbhit(), getch(), and Sleep()
 
+// Parameter declaration
 int i, j, height  =20, width = 20, gameover, score;
 int x, y, fruitx, fruity, flag;
 
+//Initialised the main function
 int main() {
     int m, n;
     setup();
